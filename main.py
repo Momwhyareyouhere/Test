e1a602f1fcacdbc23863d6852acfa04e75531961
Momwhyareyouhere <151050626+Momwhyareyouhere@users.noreply.github.com>
2024-02-22 17:53:29 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-
-def open_stonks_boats_invite():
-    url = "https://stonks.boats/invite.php?id=57SXIU.png"
-    webbrowser.open(url)
-
-if __name__ == "__main__":
-    open_stonks_boats_invite()
